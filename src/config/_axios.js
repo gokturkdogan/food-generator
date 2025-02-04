c97c2291e725a2f4c73c293de238cf1181aa82fc
göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-02-04 14:36:34 +0300

    api url changed

--- src/config/_axios.js
+++ src/config/_axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_API_URL= "https://672f4e42229a881691f2822a.mockapi.io";
+const BASE_API_URL= "https://67a1f9b4409de5ed5253ca14.mockapi.io/";
 
 const Services = axios.create({baseURL: String(BASE_API_URL)});
  
