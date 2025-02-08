97b9c9844f1981fab084a84ce2f8b386ad83e859
göktürk DOĞAN <gokturk.dogan@hotmail.com>
2025-02-08 10:32:05 +0300

    code beautify

--- src/components/product-list/Item.vue
+++ src/components/product-list/Item.vue
@@ -1,119 +1,133 @@
 <template>
-    <div class="productItem">
-      <div v-if="loader" class="productItem__loader">
-        <img class="productItem__gif" src="../../assets/images/loaders/loader.gif" alt="loader">
-      </div>
-      <div v-else class="productItem__content">
-        <img :src="product.image" alt="productItem" class="productItem__image">
-        <div class="productItem__data">
-            <div class="productItem__text">
-                <span class="productItem__title">
-                    {{ product.name }}
-                </span>
-                <span class="productItem__subtitle">
-                  {{ product.categoryName }} / {{ product.subCategoryName }}
-                </span>
-            </div>
-            <div v-if="product.isFavorite" class="productItem__action" @click="deleteFavorites(product.productId)">
-                <FavIcon />
-            </div>
-            <div v-else class="productItem__action" @click="addFavorites(product.productId)">
-                <FavIconEmpty />
-            </div>
+  <div class="productItem">
+    <div v-if="loader" class="productItem__loader">
+      <img
+        class="productItem__gif"
+        src="../../assets/images/loaders/loader.gif"
+        alt="loader"
+      />
+    </div>
+    <div v-else class="productItem__content">
+      <img :src="product.image" alt="productItem" class="productItem__image" />
+      <div class="productItem__data">
+        <div class="productItem__text">
+          <span class="productItem__title">
+            {{ product.name }}
+          </span>
+          <span class="productItem__subtitle">
+            {{ product.categoryName }} / {{ product.subCategoryName }}
+          </span>
+        </div>
+        <div
+          v-if="product.isFavorite"
+          class="productItem__action"
+          @click="deleteFavorites(product.productId)"
+        >
+          <FavIcon />
+        </div>
+        <div
+          v-else
+          class="productItem__action"
+          @click="addFavorites(product.productId)"
+        >
+          <FavIconEmpty />
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   <script>
-  import FavIcon from '../../assets/images/icons/favorite-icon.vue';
-  import FavIconEmpty from '../../assets/images/icons/favorite-empty-icon.vue';
-  export default {
-    name: "ProductItem",
-    data() {
-      return {
-        loader: false
-      }
+import FavIcon from "../../assets/images/icons/favorite-icon.vue";
+import FavIconEmpty from "../../assets/images/icons/favorite-empty-icon.vue";
+export default {
+  name: "ProductItem",
+  data() {
+    return {
+      loader: false,
+    };
+  },
+  components: {
+    FavIcon,
+    FavIconEmpty,
+  },
+  props: {
+    product: {
+      type: Object,
+      required: true,
     },
-    components: {
-      FavIcon,
-      FavIconEmpty
+  },
+  created() {},
+  methods: {
+    async deleteFavorites(productId) {
+      this.loader = true;
+      await this.$store.dispatch("favorites/deleteFavorites", { productId });
+      this.loader = false;
     },
-    props: {
-      product: {
-        type: Object,
-        required: true,
-      },
+    async addFavorites(productId) {
+      this.loader = true;
+      await this.$store.dispatch("favorites/addFavoritesFromProductList", {
+        productId,
+      });
+      this.loader = false;
     },
-    created() {},
-    methods: {
-      async deleteFavorites(productId) {
-        this.loader = true;
-        await this.$store.dispatch('favorites/deleteFavorites', { productId });
-        this.loader = false;
-      },
-      async addFavorites(productId) {
-        this.loader = true;
-        await this.$store.dispatch('favorites/addFavoritesFromProductList', { productId });
-        this.loader = false;
-      }
-    }
-  };
-  </script>
+  },
+};
+</script>
   <style lang="scss" scoped>
-  .productItem {
-    position: relative;
-    background-color: $white;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+.productItem {
+  position: relative;
+  background-color: $white;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: fit-content;
+  height: 130px;
+  padding: 10px;
+  border-radius: 15px;
+  margin-top: 50px;
+
+  &__image {
+    width: 130px;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  &__loader {
+    display: flex;
+    justify-content: center;
+  }
+
+  &__gif {
+    width: 150px;
+  }
+
+  &__data {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 75px;
+    height: 100%;
+    padding-right: 20px;
+  }
+
+  &__text {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    height: fit-content;
-    height: 130px;
-    padding: 10px;
-    border-radius: 15px;
-    margin-top: 50px;
-  
-    &__image {
-      width: 130px;
-      position: absolute;
-      left: 0;
-      top: 50%;
-      transform: translate(-50%, -50%);
-    }
-  
-    &__loader {
-      display: flex;
-      justify-content: center;
-    }
-  
-    &__gif {
-      width: 150px;
-    }
-  
-    &__data {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-left: 75px;
-      height: 100%;
-      padding-right: 20px;
-    }
-  
-    &__text {
-      display: flex;
-      flex-direction: column;
-    }
-  
-    &__title {
-      font-weight: 600;
-      letter-spacing: 1px;
-      font-size: 22px;
-    }
-  
-    &__subtitle {
-      color: $subtitle;
-      font-size: 16px;
-      margin-top: 15px;
-    }
   }
-  </style>
+
+  &__title {
+    font-weight: 600;
+    letter-spacing: 1px;
+    font-size: 22px;
+  }
+
+  &__subtitle {
+    color: $subtitle;
+    font-size: 16px;
+    margin-top: 15px;
+  }
+}
+</style>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,36 +1,41 @@
 <template>
-    <div class="products" :class="{ '-loader': loader }">
-      <img class="products__loader" v-if="loader" src="../assets/images/loaders/loader.gif" alt="loader">
-      <div v-else>
-        <List />
-      </div>
+  <div class="products" :class="{ '-loader': loader }">
+    <img
+      class="products__loader"
+      v-if="loader"
+      src="../assets/images/loaders/loader.gif"
+      alt="loader"
+    />
+    <div v-else>
+      <List />
     </div>
+  </div>
 </template>
 <script>
-import List from '../components/product-list/List.vue';
-import EmptyIcon from '../assets/images/icons/header-logo-icon.vue';
+import List from "../components/product-list/List.vue";
+import EmptyIcon from "../assets/images/icons/header-logo-icon.vue";
 export default {
   name: "ProductList",
   data() {
     return {
-      loader: true
-    }
+      loader: true,
+    };
   },
   components: {
     List,
-    EmptyIcon
+    EmptyIcon,
   },
   async created() {
-    await this.$store.dispatch('category/getProductList');
+    await this.$store.dispatch("category/getProductList");
     setTimeout(() => {
       this.loader = false;
     }, 1000);
   },
   computed: {
     products() {
-      return this.$store.getters['category/getProducts']
-    }
-  }
+      return this.$store.getters["category/getProducts"];
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
